ddcbfa7ec6191f19a9f8bcd9f7de1086866b7051
chenyifei <chenyifei@letv.com>
2015-01-21 20:50:42 +0800

    up

--- aksk.lua
+++ aksk.lua
@@ -7,29 +7,6 @@ local STRING = require "resty.string"
 DB_NAME = "aksk"
 COL_NAME = "manager"
 
-local router = require('mch.router')
-router.setup()
-
-map('.*', 'handler')
-
-local function handler(req, resp)
-	local conn = connectdb()
-	if not conn then
-		ngx.exit(500)
-	end
-	if req.method == 'GET' then
-		code = get(req, resp, conn)
-	elseif req.method == 'DELETE' then
-		code = delete(req, resp, conn)
-	elseif req.method == 'POST' then
-		code = create(req, resp, conn)
-	end
-	conn:close()
-	if code ~= 200 then
-		ngx.exit(code)
-	end
-end
-
 local function connectdb()
 	local db_host = ngx.var.aksk_manager_host or "127.0.0.1"
 	local db_port = ngx.var.aksk_manager_port or 27017
@@ -43,58 +20,139 @@ local function connectdb()
 	return conn
 end
 
-local function get(req, resp, conn)
-    if not req.uri_args then
-        return 404
-	end
+local function onResponse(errno, errmsg, data, httpcode)
+	local reply = {}
+	reply.errno = errno
+	if errmsg then reply.errmsg = errmsg end
+	if data then reply.data = data end
+   	ngx.say(JSON.encode(reply))
+	return httpcode
+end
+
+-- POST /api/v1/app
+local function addApp(conn)
+    --ngx.req.read_body()
+	--local data = ngx.req.get_body_data()
+	--j = JSON.decode(data)
+	--if not j then
+    --    ngx.log(ngx.ERR, "invalid data: ", data)
+	--	return 401
+	--end
+	local u1 = UUID:generate()
+    local appid = "id_" .. u1
+    u1 = UUID:generate()
+    md5 = MD5:new()
+    md5:update(u1)
+    local ak = "ak_" .. STRING.to_hex(md5:final())
+    
+	u1 = UUID:generate()
+    md5 = MD5:new()
+    md5:update(u1)
+    local sk = "sk_" .. STRING.to_hex(md5:final())
+
+    local data = {appid=appid, ak=ak, sk=sk}
     local col = conn:new_db_handle(DB_NAME):get_col(COL_NAME)
-    local r = col:find_one({ak=req.uri_args["ak"]}, {sk=1})
+	--local r, err = col:update({ak=ak}, doc, 1, 0, true)
+    local r, err = col:insert({data}, false, true)
     if not r then
-        return 404
+		return onResponse(10001, "internal error", nil, 500)
 	end
-    resp:writeln(JSON.encode({sk=r["sk"]}))
-	return 200
+	return onResponse(0, nil, data, 200)
 end
 
-local function delete(req, resp, conn)
+-- DELETE /api/v1/app
+local function delApp(conn)
+	ngx.req.read_body()
+	local data = ngx.req.get_body_data()
+	j = JSON.decode(data)
+	if not j then
+		return onResponse(10001, "invalid POST data", nil, 401)
+	end
+	if not j["appid"] then
+		return onResponse(10001, "missing 'appid'", nil, 401)
+	end
     local col = conn:new_db_handle(DB_NAME):get_col(COL_NAME)
-    if req.uri_args then
-        col:delete({ak=req.uri_args["ak"]})
-    end
-	return 200
+    col:delete({appid=args["appid"]})
+	return onResponse(0, nil, nil, 200)
 end
 
-local function create(req, resp, conn)
-    req:read_body()
-    if not req.post_args then
-        return 400
+-- GET /api/v1/app
+local function getApp(conn)
+	args = ngx.req.get_uri_args()
+    if not args then
+		return onResponse(10001, "invalid GET args", nil, 401)
 	end
-	m = JSON.decode(req.post_args)
-	if not m then
-		return 401
+	if not args["appid"] then
+		return onResponse(10001, "missing 'appid'", nil, 401)
 	end
-
-    local token = m["sso_token"]
-	if not token then
-		return 401
-	end
-
-    local sk = UUID:generate()
-    local md5 = MD5:new()
-    md5:update(token)
-
-    local ak = STRING.to_hex(md5:final())
-    local sk = UUID:generate()
-    local data = {}
-    data["$set"] = {sk=sk}
-
     local col = conn:new_db_handle(DB_NAME):get_col(COL_NAME)
-    local r, err = col:update({ak=ak}, data, 1, 0, true)
+    local r = col:find_one({appid=args["appid"]}, {ak=1, sk=1})
     if not r then
-        ngx.log(ngx.ERR, "failed to gen aksk", err)
-        return 500
+		return onResponse(10001, "no such 'appid'", nil, 404)
 	end
-    resp:writeln(JSON.encode({ak=ak, sk=sk}))
-	return 200
+	data = {appid=args["appid"], ak=r["ak"], sk=r["sk"]}
+	return onResponse(0, nil, data, 200)
 end
+
+-- GET /api/v1/aksk/check
+local function checkAksk(conn)
+	args = ngx.req.get_uri_args()
+    if not args then
+		return onResponse(10001, "invalid GET args", nil, 401)
+	end
+	if not args["appid"] or not args["ak"] or not args["sk"] then
+		return onResponse(10001, "missing args", nil, 401)
+	end
+    local col = conn:new_db_handle(DB_NAME):get_col(COL_NAME)
+    local r = col:find_one({appid=args["appid"], ak=args["ak"], sk=args["sk"]}, {ak=1, sk=1})
+    if not r then
+		return onResponse(0, nil, {result="0"}, 200)
+	end
+	return onResponse(0, nil, {resultl="1"}, 200)
+end
+
+local function handleApp(method, uri, conn)
+	if method == 'POST' then
+		code = addApp(conn)
+	elseif method == 'DELETE' then
+		code = delApp(conn)
+	else
+		onResponse(10001, "unsupport method", nil, 401)
+		code = 401
+	end
+	return code
+end
+
+local function handleAksk(method, uri, conn)
+	if method == 'GET' then
+		code = checkAksk(conn)
+	else
+		onResponse(10001, "unsupport method", nil, 401)
+		code = 401
+	end
+	return code
+end
+
+method = ngx.req.get_method()
+uri = ngx.var.uri
+
+router = {}
+router["/api/v1/app"]        = handleApp
+router["/api/v1/aksk/check"] = handleAksk
+
+handler = router[uri]
+if not handler then
+	onResponse(10001, "unknown URI", nil, 404)
+	ngx.exit(404)
+end
+
+local conn = connectdb()
+if not conn then
+	onResponse(10001, "internal error", nil, 500)
+	ngx.exit(500)
+end
+
+code = handler(method, method, conn)
+conn:close()
+ngx.exit(code)
 
